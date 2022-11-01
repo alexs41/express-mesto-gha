49c1a4a575a1572ad7e2730a9ff752583ece7e87
Aleksandr Kaigorodov <alexkaygorodov@gmail.com>
2022-11-01 12:59:27 +0600

    Реализовал Like / Dislike карточки

--- app.js
+++ app.js
@@ -82,6 +82,25 @@ app.delete('/cards/:cardId', (req, res) => {
       .then(card => res.send({ data: card }))
       .catch(err => res.status(500).send({ message: 'Произошла ошибка удаления карточки' }));
 });
+
+// LIKE CARD
+app.put('/cards/:cardId/likes', jsonParser, (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId,
+    { $addToSet: { likes: req.user._id } }, // добавить _id в массив, если его там нет
+    { new: true },// обработчик then получит на вход обновлённую запись
+  )
+    .then(card => res.send({ data: card }))// вернём записанные в базу данные
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));// данные не записались, вернём ошибку
+});
+// DISLIKE CARD
+app.delete('/cards/:cardId/likes', (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId,
+    { $pull: { likes: req.user._id } }, // убрать _id из массива
+    { new: true },
+  )
+    .then(card => res.send({ data: card }))
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка удаления карточки' }));
+});
 //------------------------------- CARD END ---------------------------------
 
 

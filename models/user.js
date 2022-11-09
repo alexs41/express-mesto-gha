da25c10729ac4ea2e1bcc88a4dd0d76c14b11190
Aleksandr Kaigorodov <alexkaygorodov@gmail.com>
2022-11-09 17:29:44 +0600

    Работа с ошибками Joi

--- models/user.js
+++ models/user.js
@@ -1,13 +1,27 @@
 import { Schema, model } from 'mongoose';
 import validator from 'validator';
 import bcrypt from 'bcrypt';
+import { Joi } from 'celebrate';
+
+const urlSchema = Joi
+  .string()
+  .url({ scheme: ['http', 'https'] })
+  .required();
+
+const nameSchema = Joi
+  .string()
+  .min(2)
+  .max(30)
+  .required();
 
 const userSchema = new Schema({
   name: { // у пользователя есть имя — опишем требования к имени в схеме:
     type: String, // имя — это строка
-    minlength: 2, // минимальная длина имени — 2 символа
-    maxlength: 30, // а максимальная — 30 символов
     default: 'Жак-Ив Кусто',
+    validate: {
+      validator: (value) => !nameSchema.validate(value).error,
+      message: () => 'Имя должно быть от 2 от 30 символов',
+    },
   },
   about: {
     type: String,
@@ -33,6 +47,38 @@ const userSchema = new Schema({
     select: false,
   },
 }, { versionKey: false });
+
+// const userSchema = new Schema({
+//   name: { // у пользователя есть имя — опишем требования к имени в схеме:
+//     type: String, // имя — это строка
+//     minlength: 2, // минимальная длина имени — 2 символа
+//     maxlength: 30, // а максимальная — 30 символов
+//     default: 'Жак-Ив Кусто',
+//   },
+//   about: {
+//     type: String,
+//     minlength: 2,
+//     maxlength: 30,
+//     default: 'Исследователь',
+//   },
+//   avatar: {
+//     type: String,
+//     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+//     validate: validator.isURL,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true,
+//     validate: validator.isEmail,
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 6,
+//     select: false,
+//   },
+// }, { versionKey: false });
 
 userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
